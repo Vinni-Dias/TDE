a3a5c3adef5854ebb02719daa19f57bccd0e2547
Vinicius Dias <94999178+Vinni-Dias@users.noreply.github.com>
2025-09-09 21:29:58 -0300

    Update and rename Exercicio 1 e 2.java to MainFilaPilha.java

--- MainFilaPilha.java
+++ MainFilaPilha.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class MainFilaPilha {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -195,3 +195,4 @@ class Fila {
         System.out.println("FIM ---");
     }
 }
+
